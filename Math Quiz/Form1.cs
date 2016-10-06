a77d09ea1e5cad0eaf02336e7503fb0cfc24ec43
Adam Gothmann <adamgothmann@gmail.com>
2016-10-06 13:17:27 -0500

    all functionality

--- Math Quiz/Form1.cs
+++ Math Quiz/Form1.cs
@@ -19,6 +19,18 @@ namespace Math_Quiz
         int addend1;
         int addend2;
 
+        //These variables store the numbers for the subtraction problem.
+        int minuend;
+        int subtrahend;
+
+        //These varialbes store the numbers for multiplication.
+        int multiplicand;
+        int multiplier;
+
+        //These varialbes store the numbers for division.
+        int dividend;
+        int divisor;
+
         //This integer variable keeps track of the remaining time
         int timeLeft;
 
@@ -34,6 +46,28 @@ namespace Math_Quiz
 
             //makes sure the NumericUpDown control's value is zero before adding values to it.
             sum.Value = 0;
+
+            //Fill in the subtraction problem.
+            minuend = randomizer.Next(1, 101);
+            subtrahend = randomizer.Next(1, minuend);
+            minusLeftLabel.Text = minuend.ToString();
+            minusRightLabel.Text = subtrahend.ToString();
+            difference.Value = 0;
+
+            //Fill in the multiplication problem.
+            multiplicand = randomizer.Next(2, 11);
+            multiplier = randomizer.Next(2, 11);
+            timesLeftLabel.Text = multiplicand.ToString();
+            timesRightLabel.Text = multiplier.ToString();
+            product.Value = 0;
+
+            //Fill in the division problem.
+            divisor = randomizer.Next(2, 11);
+            int temporaryQuotient = randomizer.Next(2, 11);
+            dividend = divisor * temporaryQuotient;
+            dividedLeftLabel.Text = dividend.ToString();
+            dividedRightLabel.Text = divisor.ToString();
+            quotient.Value = 0;
 
             //Start the timer.
             timeLeft = 30;
@@ -54,6 +88,13 @@ namespace Math_Quiz
 
         private void timer1_Tick(object sender, EventArgs e)
         {
+            if (CheckTheAnswer())
+            {
+                //If CheckTheAnswer() returns true, the user got the answer right. Stop timer and show MessageBox
+                timer1.Stop();
+                MessageBox.Show("You got all the answers right!", "Congratulations!");
+                startButton.Enabled = true;
+            }
             if (timeLeft > 0)
             {
                 //Display the new time left by updating the Time Left label.
@@ -67,7 +108,36 @@ namespace Math_Quiz
                 timeLabel.Text = "Time's up!";
                 MessageBox.Show("You didn't finish in time.", "Sorry!");
                 sum.Value = addend1 + addend2;
+                difference.Value = minuend - subtrahend;
+                product.Value = multiplicand * multiplier;
+                quotient.Value = dividend / divisor;
                 startButton.Enabled = true;
+            }
+        }
+        private bool CheckTheAnswer()
+        {
+            if ((addend1 + addend2 == sum.Value) 
+                && (minuend - subtrahend == difference.Value)
+                && (multiplicand * multiplier == product.Value)
+                && (dividend / divisor == quotient.Value))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private void answer_Enter(object sender, EventArgs e)
+        {
+            //Select the whole answer in the NumericUpDown control.
+            NumericUpDown answerBox = sender as NumericUpDown;
+            
+            if (answerBox != null)
+            {
+                int lengthOfAnswer = answerBox.Value.ToString().Length;
+                answerBox.Select(0, lengthOfAnswer);
             }
         }
     }
